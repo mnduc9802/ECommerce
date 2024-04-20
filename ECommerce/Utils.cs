8493d5fa1a758a94ad5674622fc602ff2e20360c
mnduc9802 <mnduc9802@gmail.com>
2024-04-20 16:08:09 +0700

    Add project files.

---
+++ ECommerce/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace ECommerce
+{
+    public class Utils
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter sda;
+        SqlDataReader sdr;
+        DataTable dt;
+
+        public static string getConnection()
+        {
+            return ConfigurationManager.ConnectionStrings["cs"].ConnectionString;
+        }
+
+        public static bool isValidExtension(string filename) 
+        {
+            bool isValid = false;
+            string[] fileExtension = { ".jpg", ".png", ".jpeg" };
+            foreach (string file in fileExtension)
+            {
+                if(filename.Contains(file))
+                {
+                    isValid = true;
+                    break;
+                }
+            }
+            return isValid;
+        }
+
+        public static string getUniqueId()
+        {
+            Guid guid = Guid.NewGuid();
+            return guid.ToString();
+        }
+    }
+}
